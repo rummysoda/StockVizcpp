ae1b1390dd30f3296da6b1d36fe8762030f55da5
Rummy <61748455+rummysoda@users.noreply.github.com>
2026-02-24 17:23:30 +0000

    added working textbutton

---
+++ src/TextButton.h
@@ -0,0 +1,157 @@
+#ifndef TEXTBUTTON_H
+#define TEXTBUTTON_H
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <sstream>
+
+#define DELETE_KEY 8
+#define ENTER_KEY 13
+#define ESCAPE_KEY 27
+
+enum BUTTON_STATE{IDLE = 0, HOVER = 1, PRESSED = 2, DISABLED = 3};
+
+class TextButton {
+private:
+sf::Sprite shape;
+sf::Texture Idle;
+sf::Texture Hover;
+sf::Texture Presssed;
+sf::Texture Disabled;
+int buttonState;
+sf::Font font;
+
+
+sf::Text textb;
+std::ostringstream text;
+bool isSelected = false;
+int limit = 5;
+
+//text functions
+void inputLogic(int charTyped) {
+        if (charTyped != ENTER_KEY && charTyped != ESCAPE_KEY && charTyped != DELETE_KEY) {
+            this->text << static_cast<char>(charTyped);
+        }
+        else if(charTyped == DELETE_KEY) {
+            if(this->text.str().length() > 0) {
+                deletelastChar();
+            }
+        }
+        this->textb.setString(this->text.str() + "_");
+    }
+
+    void deletelastChar() {
+        std::string t = this->text.str();
+        std::string newT = "";
+        for(int i = 0; i < t.length() - 1; i++) {
+            newT += t[i];
+        }
+        this->text.str("");
+        this->text << newT;
+
+        this->textb.setString(this->text.str());
+    }
+
+public:
+	TextButton(float x, float y) {
+		this->font.loadFromFile("pt-root-ui_vf.ttf");
+		this->Idle.loadFromFile("Bdefault.png");
+		this->Hover.loadFromFile("BHovered.png");
+		this->Presssed.loadFromFile("BPressed.png");
+		this->Disabled.loadFromFile("BDisabled.png");
+		this->buttonState = IDLE;
+
+		this->shape.setTexture(this->Idle);
+		this->shape.setPosition({x,y});
+
+		this->textb.setPosition({x,y+5});
+		this->textb.setFont(this->font);
+		this->textb.setCharacterSize(20);
+
+		 if(isSelected) {
+            this->textb.setString("_");
+        }
+        else {
+            this->textb.setString("");
+        }
+	}
+
+
+	// text settings
+	void setSelected(bool sel) {
+        isSelected = sel;
+        if(!sel) {
+            std::string t = this->text.str();
+            std::string newT = " ";
+            for(int i = 0; i < t.length() ; i++) {
+                newT += t[i];
+            }
+            this->textb.setString(newT);
+        }
+		else {
+			this->textb.setString(this->text.str() + "_");
+		}
+    }
+
+    std::string getText() {
+        return this->text.str();
+    }
+
+    void drawTo(sf::RenderWindow &window) {
+        window.draw(this->textb);
+    }
+
+
+    void typedOn(sf::Event &input) {
+        if(isSelected) {
+            int charTyped = input.text.unicode;
+            if(charTyped < 128) {
+
+                if(this->text.str().length() <= limit) {
+                    inputLogic(charTyped);
+                }
+                else if (charTyped == DELETE_KEY) {
+                	deletelastChar();
+                }
+            }
+        }
+    }
+
+    // button settings
+	bool isPressed() const {return this->buttonState == PRESSED;}
+	bool isHover() const {return this->buttonState == HOVER;}
+	void update(const sf::RenderWindow& window) {
+		sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+		if(this->shape.getGlobalBounds().contains(mousePos)) {
+			this->shape.setTexture(this->Hover);
+			this->buttonState = HOVER;
+			this->textb.setFillColor(sf::Color::White);
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+				//selected
+				this->shape.setTexture(this->Presssed);
+				this->buttonState = PRESSED;
+				this->textb.setFillColor(sf::Color(56,23,216));
+				this->setSelected(true);
+			}
+		}
+			else {
+				this->shape.setTexture(this->Idle);
+				this->buttonState = IDLE;
+					this->textb.setFillColor(sf::Color::Green);
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+					this->setSelected(false);
+				}
+				if (!this->shape.getGlobalBounds().contains(mousePos) && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {this->setSelected(false);}
+			}
+			// make sure in the while loop to take even for textenetered to set textbox[i]->textb,typedOn(event);
+	}
+
+	void render(sf::RenderTarget& target){
+		target.draw(this->shape);
+		target.draw(this->textb);
+
+	}
+
+};
+#endif //TEXTBUTTON_H
--- src/window.cpp
+++ src/window.cpp
@@ -6,45 +6,56 @@
 #include "imgui.h"
 #include "imgui-SFML.h"
 #include "implot.h"
-
 #include "stocksgui.h"
+#include "TextButton.h"
 
-void runWindow()
-{
+
+void runWindow() {   //style none gets rid of titlebar sf::Style::None
     sf::RenderWindow window(sf::VideoMode(1920, 1000), "Stock Visualizer");
     ImGui::SFML::Init(window);
     ImPlot::CreateContext();
 
     window.setFramerateLimit(60);
-
     sf::Font font;
     font.loadFromFile("pt-root-ui_vf.ttf");
 
-    sf::Text myt;
-    myt.setFont(font);
-    myt.setString("Hello World!");
-    myt.setCharacterSize(40);
-    myt.setPosition(300,400);
-    myt.setFillColor(sf::Color::Green);
+    std::vector<TextButton*> buttonVec;
+    float keyTimeMax = 1000.f;
+    float keyTime = keyTimeMax;
+
+
+
 
     sf::Clock deltaClock;
 
     while (window.isOpen())
     {
         sf::Event event;
+
         while (window.pollEvent(event))
         {
+
             ImGui::SFML::ProcessEvent(window, event);
-            if (event.type == sf::Event::Closed) {window.close();}
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {window.close();}
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::TextEntered:
+                    for (size_t i = 0; i< buttonVec.size(); i++) {buttonVec[i]->typedOn(event);}
+                    break;
+            }
+
         }
+
 
         ImGui::SFML::Update(window,deltaClock.restart());
 
         // imgui test
-        ImGui::Begin("Test Panel");
+        /* ImGui::Begin("Test Panel");
         ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
         ImGui::End();
+
+
 
 
         // implot test
@@ -57,15 +68,56 @@ void runWindow()
         }
         ImGui::End(); */
 
+        ImGui::Begin("checkbox");
+        static bool toggle = false;
+        ImGui::Checkbox("toggle", &toggle);
+        ImGui::End();
+
         Demo_CustomPlottersAndTooltips();
 
+
+
+        // button
+        sf::Vector2f mousePosView = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::B) && keyTime >= keyTimeMax)
+        {
+            float xx;
+            float yy;
+            xx = 1500 + buttonVec.size();
+            yy = 150 + buttonVec.size() * 100;
+            buttonVec.push_back(new TextButton(xx,yy));
+        }
+
+        for (size_t i = 0; i < buttonVec.size(); i++) {
+            buttonVec[i]->update(window);
+            if (buttonVec[i]->isPressed()) {
+                for (size_t j = 0; j < buttonVec.size(); j++) {
+                    if (j != i) {
+                        buttonVec[j]->setSelected(false);
+                    }
+                }
+            }
+        }
+
+        if (keyTime < keyTimeMax)
+        {
+            keyTime += 1.f;
+        }
+        window.clear(sf::Color::Black);
+        for (size_t i = 0; i < buttonVec.size(); i++)
+        {
+            buttonVec[i]->render(window);
+        }
         //render
 
-        window.clear(sf::Color::Black);
         ImGui::SFML::Render(window);
-        window.draw(myt);
-
-
+        //window.draw(myt);
         window.display();
+
+    }
+    for (size_t i = 0; i < buttonVec.size(); i++)
+    {
+        delete buttonVec[i];
     }
 }
